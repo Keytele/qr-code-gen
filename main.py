1d8bcf5011566dc24350f9c96875acacc3d0f022
Joshua Aldridge <joshu459@hotmail.com>
2025-08-30 19:59:55 +1000

    Commenting

--- main.py
+++ main.py
@@ -8,20 +8,20 @@ import qrcode
 
 app = FastAPI() # Create FastAPI instance
 
-class Url(BaseModel):
+class Url(BaseModel): # Defining URL through Pydantic
     url: str = Field("localhost:8000")
     
-@app.post("/generate")
+@app.post("/generate") # POST HTTP endpoint when generating QR code
 async def generate_qr_code(url: Url):
     try: 
         img = qrcode.make(url.url)
-        filename = "qrcode.png"
+        filename = "qrcode.png" # Defining name of QR Code and save location
         img.save("./qrcodes/" + filename)
         return {"message": "QR Code has been generated successfully!", "qrcode": filename}
     except Exception as e:
-        raise HTTPException(status_code=500, detail="Internal Server Error")
+        raise HTTPException(status_code=500, detail="Internal Server Error") # QR code generation fails due to the server
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000) # Run the uvicorn server for FastAPI
     
